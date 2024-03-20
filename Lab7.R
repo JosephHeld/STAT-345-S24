db9ff29e7ca73920ac670d9731e4421de321f3dd
JosephHeld <joeheld04@gmail.com>
2024-03-20 15:11:21 -0500

    Added y = 52

--- Lab7.R
+++ Lab7.R
@@ -1,6 +1,7 @@
-joe <- data.frame(c("Joe is Cool",
+joe <- matrix(c("Joe is Cool",
                     "Joe is Awesome",
                     "Joe is Crazy",
                     "Joe is Smart"))
 
 x<-34
+y <- 52
